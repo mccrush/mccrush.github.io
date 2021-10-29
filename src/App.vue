8a24628cbc2819f1f62fcf102ef6ecdbadd2d6bf
mccrush <mccrush2027@gmail.com>
2021-10-29 14:26:16 +0700

    Settings cards

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <template>
-  <div class="container border">
-    <div class="row">
-      <div class="col-12">1</div>
-      <div class="col-12">2</div>
-      <div class="col-12">3</div>
+  <div class="container-lg mt-4 mb-4">
+    <div class="row border">
+      <div class="col-12">
+        <h1>mccrush apps</h1>
+      </div>
     </div>
+    <div class="row border">
+      <div class="col-12">2</div>
+    </div>
+    <h4 class="text-center">Приложения на домене</h4>
+    <Apps />
   </div>
 </template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Apps from '@/components/Apps'
+export default {
+  components: {
+    Apps
+  }
+}
+</script>
+
+<style>
+body {
+  color: #f2f2f2;
+  background-color: #32425a;
+}
+</style>
+  
--- src/data/apps.js
+++ src/data/apps.js
@@ -1,3 +1,5 @@
 export default [
-  { title: '', description: '', alias: '' },
-  { title: '', description: '', alias: '' }]
+  { title: 'Info Math', description: '', alias: 'info' },
+  { title: 'Electro Shit', description: '', alias: 'shit' },
+  { title: 'Comp Master', description: '', alias: 'comp' },
+  { title: 'Em 5 electric', description: '', alias: 'em5' }]
