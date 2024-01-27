e2c720b74944d77e7e98cd34e31ec4b77f0f134b
mccrush <mccrush2027@gmail.com>
2024-01-27 17:06:30 +0700

    Settinhs view icons

--- src/modules/services/components/ui/ServicesCard.vue
+++ src/modules/services/components/ui/ServicesCard.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="col-12 col-sm-6 col-md-4 mt-4 ps-2 pe-2">
     <div class="border-top-light d-block shadow-sm rounded text-start p-4 pt-3">
-      <h4 class=""><component :is="icon" class="" /></h4>
+      <h4>
+        <component :is="icon" class="" />
+      </h4>
       <h5 class="">{{ app.title }}</h5>
       <p class="m-0" v-html="app.description"></p>
     </div>
@@ -31,10 +33,3 @@ export default {
   }
 }
 </script>
-
-
-<style scoped>
-/* .mc-card-icon {
-  width: 32px;
-} */
-</style>
