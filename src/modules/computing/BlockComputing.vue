e515ee1666b498741fda089792eea1b1bf5ef303
mccrush <mccrush2027@gmail.com>
2024-01-27 09:31:54 +0700

    Settings adaptive

--- src/modules/computing/BlockComputing.vue
+++ src/modules/computing/BlockComputing.vue
@@ -2,14 +2,65 @@
   <div class="f4">
     <div class="container-960 row text-center m-0 m-auto ps-2 pt-5 pe-2 pb-5">
       <h3 id="computing">Услуги настройки ПК</h3>
-      <div class="col-12 col-md-6 m-auto p-0 pt-3">
+
+      <div class="col-12 d-md-none m-auto p-0 pt-3">
         <div class="shadow rounded-2 p-2">
           <div
             v-for="comp in computing"
             :key="comp.id"
             class="d-flex justify-content-between m-3"
           >
-            <div>
+            <div class="text-start pe-2">
+              {{ comp.title }}
+              <button
+                v-if="comp.desc"
+                type="button"
+                class="btn btn-sm ms-1 p-0 ps-2 pe-2"
+                data-bs-toggle="popover"
+                data-bs-placement="bottom"
+                :data-bs-content="comp.desc"
+              >
+                ?
+              </button>
+            </div>
+            <div>{{ comp.price }}&nbsp;₽</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-6 d-none d-md-block p-0 pt-3">
+        <div class="shadow rounded-2 p-2">
+          <div
+            v-for="comp in computing.slice(0, 4)"
+            :key="comp.id"
+            class="d-flex justify-content-between m-3"
+          >
+            <div class="text-start pe-2">
+              {{ comp.title }}
+              <button
+                v-if="comp.desc"
+                type="button"
+                class="btn btn-sm ms-1 p-0 ps-2 pe-2"
+                data-bs-toggle="popover"
+                data-bs-placement="bottom"
+                :data-bs-content="comp.desc"
+              >
+                ?
+              </button>
+            </div>
+            <div>{{ comp.price }}&nbsp;₽</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-6 d-none d-md-block p-0 pt-3">
+        <div class="shadow rounded-2 p-2">
+          <div
+            v-for="comp in computing.slice(4)"
+            :key="comp.id"
+            class="d-flex justify-content-between m-3"
+          >
+            <div class="text-start pe-2">
               {{ comp.title }}
               <button
                 v-if="comp.desc"
